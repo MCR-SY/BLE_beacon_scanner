023fabb837abace6a345672f1146f43226289dcb
Vaibhav Singh <46848906+vsvai@users.noreply.github.com>
2025-11-22 18:57:42 +0530

    Add files via upload

---
+++ BLE_beacon_scanner.ino
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+#include <WiFi.h>
+//ble libraries
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+// extra libraries
+#include "time.h"
+
+// wifi credentials
+const char* ssid = "Airtel_vsvai";
+const char* password = "lenovoturbo";
+
+int scanTime = 5; // seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str()); //full data
+
+    // check device name
+    if (advertisedDevice.haveName()) {
+      Serial.print("Device name: ");
+      Serial.println(advertisedDevice.getName().c_str());
+      Serial.println("");
+    }
+
+    // print mac_address
+    String macAddress = advertisedDevice.getAddress().toString().c_str();
+    Serial.print("MAC Address: ");
+    Serial.println(macAddress);
+
+    // print rssi
+    int rssi = advertisedDevice.getRSSI();   // <-- signal strength in dBm
+    Serial.printf("RSSI: %d dBm\n", rssi);
+
+    // check for serviceUUID
+    if (advertisedDevice.haveServiceUUID()) {
+      BLEUUID devUUID = advertisedDevice.getServiceUUID();
+      Serial.print("ServiceUUID: ");
+      Serial.println(devUUID.toString().c_str());
+      Serial.println("");
+    }
+
+    // Get current time
+    struct tm timeinfo;
+    if (getLocalTime(&timeinfo)) {
+      Serial.printf("ctime: [%02d:%02d:%02d] \n\n",
+                    timeinfo.tm_hour,
+                    timeinfo.tm_min,
+                    timeinfo.tm_sec);
+    } else {
+      Serial.print("[time unavailable] \n\n");
+    }
+
+  }
+};
+
+void setup() {
+  // initializing Serial monitor
+  Serial.begin(115200);
+  Serial.println();
+
+  // initializing wifi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to wifi with IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Init NTP time
+  configTime(19800, 0, "time.google.com"); // GMT+5:30 for India
+  struct tm timeinfo;
+  while (!getLocalTime(&timeinfo)) {
+    Serial.println("Waiting for NTP...");
+    delay(1000);
+  }
+  Serial.println("NTP time acquired!\n");
+
+  // BLE scanning setup
+  Serial.println("Now scanning ble devices...");
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); // create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); // faster, but consumes more power
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99); // <= setInterval
+}
+
+void loop() {
+  // start scanning
+  BLEScanResults *foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Device found: ");
+  Serial.println(foundDevices->getCount());
+  Serial.println("Scan done!\n\n");
+  pBLEScan->clearResults(); // delete results from BLEScan buffer to free memory
+  delay(2000);
+}
